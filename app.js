97b2aa244be5ae14d26d6861ecd486b51d6f0d72
Umer Hassan <umerhassan@Umers-MacBook-Pro.local>
2022-09-10 01:17:10 -0400

    chore: enable cors

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 
 const indexRouter = require('./routes/index');
 const mediaRouter = require('./routes/mediaRoutes');
 
 const app = express();
 
+app.use(cors({ origin: '*' }));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
